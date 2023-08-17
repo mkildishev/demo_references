609839e40e2fb13e96bcf81b844c1c13e1e22797
Maksim Kildishev <mkildishev@yandex.com>
2023-08-17 21:09:52 +0600

    add: soft reference demo

---
+++ src/main/java/com/example/demo/LogicController.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import com.example.demo.model.AutocompletionTextField;
+import javafx.fxml.FXML;
+
+public class LogicController {
+
+    @FXML
+    private AutocompletionTextField textField;
+
+    @FXML
+    public void addText() {
+        if (textField.getCache() == null) {
+            textField.setCache();
+        }
+        textField.getCache().get().add(textField.textProperty().get());
+        textField.clear();
+    }
+
+    @FXML
+    public void addGarbage() {
+
+        for (int i =  0; i < 1000000; ++i) {
+            if (textField.getCache() == null) {
+                textField.setCache();
+            }
+            textField.getCache().get().add(new String("thrash" + i));
+        }
+        System.out.println("done");
+
+    }
+}
